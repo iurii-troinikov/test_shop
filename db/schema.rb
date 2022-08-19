fea001e7c756b306cfdd331e310dba06dcccb5f6
iurii-troinikov <yurkel12@gmail.com>
2022-08-19 15:27:47 +0300

    Lesson 1: create User, Product models. Controller and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_092504) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_112541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
+  create_table "products", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "users"
 end
