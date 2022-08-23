7692d08860b15deb584f49601061b21c53641f3f
iurii-troinikov <yurkel12@gmail.com>
2022-08-23 13:12:20 +0300

    Lesson 2: edited models as: :commentable

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable
   has_many :product_categories
   has_many :categories, through: :product_categories
   validates :categories, uniqueness: { scope: :product_id }
