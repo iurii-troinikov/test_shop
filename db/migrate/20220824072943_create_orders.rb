9a6aef8593c29634bc7824a6529db31c589cd0aa
iurii-troinikov <yurkel12@gmail.com>
2022-08-24 13:28:42 +0300

    Lesson 3: create migrations and added views

--- db/migrate/20220824072943_create_orders.rb
+++ db/migrate/20220824072943_create_orders.rb
@@ -1,6 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.integer :status
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -44,13 +44,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_073016) do
   end
 
   create_table "order_items", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "order_id", null: false
+    t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "product_categories", force: :cascade do |t|
@@ -78,6 +86,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_073016) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
   add_foreign_key "products", "users"
