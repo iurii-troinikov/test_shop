4723a855244c2ddcb2ac15f1f7487c44ae3af29b
iurii-troinikov <yurkel12@gmail.com>
2022-08-25 13:41:19 +0300

    Lesson 4: generate Devise Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   helper_method :current_order
   helper_method :current_user
+
 
   def current_order
     Order.find_or_create_by(user: current_user, status: 'in_progress')
