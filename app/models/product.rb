428e21b42a3fed02ee2bb6ad7c7be5a37daef823
iurii-troinikov <yurkel12@gmail.com>
2022-08-26 12:40:25 +0300

    Lesson 5: implemented image processing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Product < ApplicationRecord
   belongs_to :user
 
@@ -7,5 +5,13 @@ class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
 
-  validates :title, :description, presence: true
+  validates :title, presence: true
+  # validates :images, file_size: { less_than_or_equal_to: 4.kilobytes, message: "Please Check File Size" },
+  #           file_content_type: { allow: %w[image/jpeg image/jpg image/png image/gif],
+  #                                message: "Please Check File Format"}
+
+  has_many_attached :images do |attachable|
+    attachable.variant :thumbnail, resize: "100x100"
+    attachable.variant :large, resize: "250x250"
+  end
 end
